d5245401390762195762ac636d626c6686b70503
Thomas Krampl <thomas@krampl.no>
2023-06-24 15:24:35 +0200

    Release pre.6

--- assembly/suffiks/memory.ts
+++ assembly/suffiks/memory.ts
@@ -1,7 +1,7 @@
-export function Malloc(size: usize): usize {
+export function malloc(size: usize): usize {
   return heap.alloc(size);
 }
 
-export function Free(ptr: usize): void {
+export function free(ptr: usize): void {
   heap.free(ptr);
 }
